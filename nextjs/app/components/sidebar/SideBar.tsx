14f6f31a4a499371fe75e0d1e1adb67c8acad272
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-12-30 11:51:58 +0900

    textarea

--- nextjs/app/components/sidebar/SideBar.tsx
+++ nextjs/app/components/sidebar/SideBar.tsx
@@ -1,8 +1,20 @@
-import styles from "./SideBar.module.css"
-
+import { useState } from "react";
+import styles from "./SideBar.module.css";
 
 export function SideBar() {
+  const [itemNames, setItemNames] = useState(`a
+b
+c`);
+
   return (
-    <div className={styles.component}></div>
-  )
+    <div className={styles.component}>
+      <textarea
+        className={styles.textarea}
+        autoComplete={"on"}
+        onChange={(x) => setItemNames(x.target.value)}
+        value={itemNames}
+        wrap="off"
+      />
+    </div>
+  );
 }
